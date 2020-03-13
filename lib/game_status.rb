1bb04ee005516a2c31ca00013e4c088d5253427e
Lesly Juarez <leslyj2@gmail.com>
2020-03-13 13:34:20 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,4 +3,10 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
+WIN_COMBINATIONS = [
+  [0,1,2], # Top row
+  [3,4,5], # Middle row
+  [6,7,8], # Last row
+  ]
+
 # Define your WIN_COMBINATIONS constant
